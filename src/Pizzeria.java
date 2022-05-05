bee280e9021274624d319b7a9921401486c42a4c
sergey_kovalenko <sergey_kovalenko2107@ukr.net>
2022-05-05 09:57:24 +0300

    New commit

--- src/Pizzeria.java
+++ src/Pizzeria.java
@@ -1,5 +1,5 @@
 /**
- * Main method fo Task 2 
+ * Main method fo Task 2
  */
 public class Pizzeria {
 
@@ -8,5 +8,10 @@ public class Pizzeria {
         order1.setOrder("Pepperoni", "BBQ", "Vegan", "My own");
         order1.setPizzaList();
         order1.printOrderInfo();
+
+        Order order2 = new Order();
+        order2.setOrder("BBQ", "BBQ", "Pepperoni", "Vegan", "Cheese", "Cheese", "My own");
+        order2.setPizzaList();
+        order2.printOrderInfo();
     }
 }
